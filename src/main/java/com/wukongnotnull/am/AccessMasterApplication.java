3345b49df8779737cf57e78b8aa79e2383328d53
wukongnotnull <1390128154@qq.com>
2023-12-12 11:44:18 +0800

    feat:项目初始化

---
+++ src/main/java/com/wukongnotnull/am/AccessMasterApplication.java
@@ -0,0 +1,13 @@
+package com.wukongnotnull.am;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccessMasterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccessMasterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wukongnotnull/am/AccessMasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wukongnotnull.am;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccessMasterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
